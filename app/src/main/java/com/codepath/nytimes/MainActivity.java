314cd36cc31931eb8a84dcf2cf8dcebb6263cfc0
Huy Do <stevehoang1998@gmail.com>
2021-07-09 01:30:58 -0700

    Add bottom navigation bar

--- app/src/main/java/com/codepath/nytimes/MainActivity.java
+++ app/src/main/java/com/codepath/nytimes/MainActivity.java
@@ -1,14 +1,70 @@
 package com.codepath.nytimes;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.codepath.nytimes.ui.books.BestSellerBooksFragment;
+import com.codepath.nytimes.ui.home.HomeFragment;
+import com.codepath.nytimes.ui.search.ArticleResultFragment;
+import com.codepath.nytimes.ui.settings.SettingsFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private static String HOME_TAG = "home";
+    private static String BOOKS_TAG = "books";
+    private static String ARTICLES_TAG = "articles";
+    private static String SETTINGS_TAG = "settings";
+
+    HomeFragment homeFragment = HomeFragment.newInstance();
+    ArticleResultFragment articleResultFragment = ArticleResultFragment.newInstance();
+    BestSellerBooksFragment bestSellerBooksFragment = BestSellerBooksFragment.newInstance();
+    SettingsFragment settingsFragment = SettingsFragment.newInstance();
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+
+        // handle navigation selection
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment fragment;
+                        String TAG ="";
+                        switch (item.getItemId()) {
+                            case R.id.nav_article_search:
+                                fragment = articleResultFragment;
+                                TAG = ARTICLES_TAG;
+                                break;
+                            case R.id.nav_settings:
+                                fragment = settingsFragment;
+                                TAG = SETTINGS_TAG;
+                                break;
+                            case R.id.nav_bestselling_books:
+                                fragment = bestSellerBooksFragment;
+                                TAG = BOOKS_TAG;
+                                break;
+                            case R.id.nav_home:
+                            default:
+                                fragment = homeFragment;
+                                TAG = HOME_TAG;
+                                break;
+                        }
+                        fragmentManager.beginTransaction().replace(R.id.fragment_content, fragment, TAG).commit();
+                        return true;
+                    }
+                });
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.nav_home);
     }
 }
---
+++ app/src/main/java/com/codepath/nytimes/ui/home/HomeFragment.java
@@ -0,0 +1,52 @@
+package com.codepath.nytimes.ui.home;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.codepath.nytimes.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link HomeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class HomeFragment extends Fragment {
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    public static HomeFragment newInstance() {
+        HomeFragment fragment = new HomeFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    // The onCreateView method is called when Fragment should create its View object hierarchy,
+    // either dynamically or via XML layout inflation.
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+    // This event is triggered soon after onCreateView().
+    // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        // Setup any handles to view objects here
+        // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
+    }
+}
